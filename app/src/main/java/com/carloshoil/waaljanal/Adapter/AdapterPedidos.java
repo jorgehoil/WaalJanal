7412cac744384051d6482c9b7c8b94ba99798a60
Carlos <jorgecarloshoilbriceno@gmail.com>
2023-07-13 07:59:13 -0600

    Listo Version 6.1

--- app/src/main/java/com/carloshoil/waaljanal/Adapter/AdapterPedidos.java
+++ app/src/main/java/com/carloshoil/waaljanal/Adapter/AdapterPedidos.java
@@ -8,10 +8,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
@@ -23,9 +25,15 @@ import com.carloshoil.waaljanal.FragmentPedidos;
 import com.carloshoil.waaljanal.R;
 import com.carloshoil.waaljanal.Utils.Global;
 import com.carloshoil.waaljanal.Utils.Values;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class AdapterPedidos extends RecyclerView.Adapter<AdapterPedidos.ViewHolder> {
@@ -33,10 +41,14 @@ public class AdapterPedidos extends RecyclerView.Adapter<AdapterPedidos.ViewHold
     Context context;
     List<Pedido> listPedidos;
     String cTipoPedidos;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     public AdapterPedidos(Context context, List<Pedido> listPedidos, String cTipoPedidos){
         this.context=context;
         this.listPedidos=listPedidos;
         this.cTipoPedidos=cTipoPedidos;
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference();
     }
     @NonNull
     @Override
@@ -50,20 +62,68 @@ public class AdapterPedidos extends RecyclerView.Adapter<AdapterPedidos.ViewHold
         Pedido pedido= listPedidos.get(holder.getAdapterPosition());
         if(pedido!=null)
         {
-            holder.tvNombre.setText(pedido.cNombreCliente.isEmpty()?"***":pedido.cNombreCliente);
-            holder.tvMesa.setText(pedido.cMesa.isEmpty()?"***": pedido.cMesa);
-            holder.tvDireccion.setText(pedido.cDireccion.isEmpty()?"***":pedido.cDireccion);
+            holder.tvNombre.setText(pedido.cNombreCliente.isEmpty()?"Mesa # "+pedido.cMesa:pedido.cNombreCliente);
+            holder.tvDireccion.setText(pedido.cDireccion.isEmpty()?"********":pedido.cDireccion);
             holder.tvNumeroOrden.setText("# "+pedido.cKey);
             holder.tvFecha.setText(ObtenerFecha(pedido.lFechaRegistro));
             holder.linearLayout.setOnClickListener(v->{
                 AbrirDialogoPedidoDetalle(pedido.cDetallePedido, pedido);
             });
+            holder.btnOpciones.setOnClickListener(v->{
+                PopupMenu popupMenu= new PopupMenu(context, holder.btnOpciones);
+                popupMenu.inflate(R.menu.menu_pedido);
+                popupMenu.setOnMenuItemClickListener(menuItem -> {
+                    int iIdItem=menuItem.getItemId();
+                    if(iIdItem==R.id.detallesPedido)
+                    {
+                        AbrirDialogoPedidoDetalle(pedido.cDetallePedido, pedido);
+                    }
+                    else if(iIdItem==R.id.cancelarPedido)
+                    {
+                        MuestraConfirmacionCancelar(pedido);
+                    }
+                    return false;
+                });
+                popupMenu.show();
+            });
         }
+    }
+
+    private void MuestraConfirmacionCancelar(Pedido pedido) {
+        AlertDialog.Builder alertDialog= new AlertDialog.Builder(context);
+        alertDialog.setTitle("¿Cancelar pedido?");
+        alertDialog.setIcon(R.drawable.ic_warning);
+        alertDialog.setMessage("Se procederá con la cancelación del pedido");
+        alertDialog.setPositiveButton("SI", (dialogInterface, i) ->{
+            CancelarPedido(pedido);
+        });
+        alertDialog.setNegativeButton("NO", (dialogInterface, i) -> dialogInterface.dismiss());
+        alertDialog.show();
     }
 
     @Override
     public int getItemCount() {
         return listPedidos.size();
+    }
+
+    private void CancelarPedido(Pedido pedido)
+    {
+        String cIdMenu=Global.RecuperaPreferencia("cIdMenu", context);
+        HashMap<String, Object> hashMapUpdates= new HashMap<>();
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/generados/"+ cTipoPedidos+ "/recibido/"+ pedido.cKey, null);
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/generados/"+ cTipoPedidos+ "/cancelado/"+ pedido.cKey, pedido);
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/todo/"+pedido.cKey+"/cEstatus", "cancelado");
+        hashMapUpdates.put("pedidos/"+ cIdMenu+ "/count/"+cTipoPedidos+"/data", ServerValue.increment(-1));
+        databaseReference.updateChildren(hashMapUpdates).addOnCompleteListener(task -> {
+            if(task.isSuccessful())
+            {
+                Toast.makeText(context, "El pedido fue cancelado", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Global.MostrarMensaje(context, "Error al cancelar", "Se ha presentado un error" +
+                        " al cancelar, intenta de nuevo");
+            }
+        });
     }
 
     public void EliminaItem(String cKey)
@@ -112,14 +172,13 @@ public class AdapterPedidos extends RecyclerView.Adapter<AdapterPedidos.ViewHold
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tvNombre, tvFecha, tvNumeroOrden, tvDireccion, tvMesa;
+        TextView tvNombre, tvFecha, tvNumeroOrden, tvDireccion;
         Button btnOpciones;
         LinearLayout linearLayout;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvNumeroOrden=itemView.findViewById(R.id.tvNumeroPedidoRow);
             tvNombre=itemView.findViewById(R.id.tvNombrePedidoRow);
-            tvMesa=itemView.findViewById(R.id.tvMesaPedidoRow);
             tvFecha=itemView.findViewById(R.id.tvFechaPedidoRow);
             tvDireccion=itemView.findViewById(R.id.tvDireccionPedidoRow);
             btnOpciones=itemView.findViewById(R.id.btnOpcionesPedidoRow);
--- app/src/main/java/com/carloshoil/waaljanal/BuscarActivity.java
+++ app/src/main/java/com/carloshoil/waaljanal/BuscarActivity.java
@@ -1,18 +1,22 @@
 package com.carloshoil.waaljanal;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +29,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,7 +39,7 @@ import java.util.Locale;
 
 public class BuscarActivity extends AppCompatActivity {
     TextView tvCliente, tvDireccion, tvEstatus, tvTelefono, tvPrecio;
-    Button btnCancelar, btnCmabiarEstatus, btnBuscar;
+    Button btnOpciones, btnBuscar;
     RecyclerView recyclerViewProd;
     ProgressBar pbCarga;
     FirebaseDatabase firebaseDatabase;
@@ -43,6 +48,10 @@ public class BuscarActivity extends AppCompatActivity {
     EditText edBuscar;
     ImageView imageViewTipo;
     AdapterPedidoDetalle adapterPedidoDetalle;
+    MenuItem itemCancelar, itemRecibir, itemTomar;
+    HashMap<String, String> hashInfo= new HashMap<>();
+    Pedido pedido;
+    PopupMenu popupMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,13 +74,30 @@ public class BuscarActivity extends AppCompatActivity {
         tvEstatus=findViewById(R.id.tvEstatusDialogBuscarPed);
         tvTelefono=findViewById(R.id.tvTelDialogBuscarPed);
         tvPrecio=findViewById(R.id.tvTotalDialogBuscarPedido);
-        btnCancelar=findViewById(R.id.btnCancelarPedidoDialgoBuscarPed);
         edBuscar=findViewById(R.id.edNumOrdenDialogBuscar);
-        btnCmabiarEstatus=findViewById(R.id.btnTomarRecDialgoBusPed);
         btnBuscar=findViewById(R.id.btnBuscarPedidoDialogBuscar);
         pbCarga=findViewById(R.id.pbCargaBuscaPed);
         imageViewTipo=findViewById(R.id.ivTipoDialogBuscarPed);
         recyclerViewProd=findViewById(R.id.recycleDialogBuscarPed);
+        btnOpciones=findViewById(R.id.btnOpcionesBuscar);
+        popupMenu= new PopupMenu(this, btnOpciones);
+        popupMenu.inflate(R.menu.menu_pedido_busqueda);
+        popupMenu.setOnMenuItemClickListener(menuItem -> {
+            int iIdItem=menuItem.getItemId();
+            if(iIdItem==R.id.pedidoBusCancelar){
+                ConfirmaCancelar();
+            } else if(iIdItem==R.id.pedidoBusRecibir)
+            {
+                CambiarEstatusPedido("recibido");
+            } else if(iIdItem==R.id.pedidoBusTomar)
+            {
+                CambiarEstatusPedido("tomado");
+            }
+            return false;
+        });
+        btnOpciones.setOnClickListener(v->{
+            popupMenu.show();
+        });
         edBuscar.setOnKeyListener((view, i, keyEvent) -> {
             if(keyEvent.getAction()==KeyEvent.ACTION_DOWN&&i==KeyEvent.KEYCODE_ENTER){
                 if(ValidaCampo(edBuscar.getText().toString()))
@@ -88,8 +114,10 @@ public class BuscarActivity extends AppCompatActivity {
                     .child("pedidos")
                     .child(cIdMenu);
             btnBuscar.setOnClickListener(v->{
-
-
+                if(ValidaCampo(edBuscar.getText().toString()))
+                {
+                    Buscar(edBuscar.getText().toString());
+                }
             });
         }
         IniciaAdapter();
@@ -112,14 +140,60 @@ public class BuscarActivity extends AppCompatActivity {
         adapterPedidoDetalle = new AdapterPedidoDetalle(this, new ArrayList<>());
         recyclerViewProd.setAdapter(adapterPedidoDetalle);
     }
-    private void ObtenerDetalles(HashMap hashMapdata){
+    private void CambiarEstatusPedido(String cEstatusNuevo)
+    {
+        boolean lAumenta=cEstatusNuevo.equals("recibido");
+        String cEstatusAnterior=hashInfo.get("cEstatus");
+        String cTipoPedido= hashInfo.get("cTipo");
+        String cIdMenu=Global.RecuperaPreferencia("cIdMenu", this);
+        HashMap<String, Object> hashMapUpdates= new HashMap<>();
+        Log.d("DEBUGX", "pedidos/"+ cIdMenu+"/generados/"+cTipoPedido+ "/"+cEstatusAnterior+"/"+pedido.cKey);
+        Log.d("DEBUGX", "pedidos/"+ cIdMenu+"/generados/"+ cTipoPedido+ "/"+cEstatusNuevo+"/"+ pedido.cKey);
+        Log.d("DEBUGX", "pedidos/"+ cIdMenu+"/todo/"+pedido.cKey+"/cEstatus");
+        Log.d("DEBUGX", "pedidos/"+ cIdMenu+"/count/"+cTipoPedido+"/data");
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/generados/"+cTipoPedido+ "/"+cEstatusAnterior+"/"+pedido.cKey, null);
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/generados/"+ cTipoPedido+ "/"+cEstatusNuevo+"/"+ pedido.cKey, pedido);
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/todo/"+pedido.cKey+"/cEstatus", cEstatusNuevo);
+        hashMapUpdates.put("pedidos/"+ cIdMenu+"/count/"+cTipoPedido+"/data", ServerValue.increment(lAumenta?1:-1));
+        firebaseDatabase.getReference().updateChildren(hashMapUpdates).addOnCompleteListener(task -> {
+            if(task.isSuccessful())
+            {
+
+                Toast.makeText(this, "El pedido fue "+ cEstatusNuevo +" correctamente", Toast.LENGTH_SHORT).show();
+                Buscar(pedido.cKey);
+            }
+            else{
+                Global.MostrarMensaje(this, "Error al procesar", "Se ha presentado un error" +
+                        " al realizar el proceso, intenta de nuevo" + task.getException().getMessage());
+            }
+        });
+    }
+    private void ConfirmaCancelar()
+    {
+        if(pedido!=null)
+        {
+            AlertDialog.Builder alertDialog= new AlertDialog.Builder(this);
+            alertDialog.setTitle("¿Cancelar pedido?");
+            alertDialog.setIcon(R.drawable.ic_warning);
+            alertDialog.setMessage("Se procederá con la cancelación del pedido");
+            alertDialog.setPositiveButton("SI", (dialogInterface, i) ->{
+               CambiarEstatusPedido("cancelado");
+            });
+            alertDialog.setNegativeButton("NO", (dialogInterface, i) -> dialogInterface.dismiss());
+            alertDialog.show();
+        }
+        else {
+            Global.MostrarMensaje(this, "Error", "Debe ingresar un número de orden");
+        }
+
+    }
+    private void ObtenerDetalles(){
         String cEstatus, cTipo, cIdOrden;
-        cEstatus=hashMapdata.get("cEstatus").toString();
-        cIdOrden=hashMapdata.get("cIdOrden").toString();
-        cTipo=hashMapdata.get("cTipo").toString();
+        cEstatus= hashInfo.get("cEstatus");
+        cIdOrden=hashInfo.get("cIdOrden");
+        cTipo=hashInfo.get("cTipo");
         databaseReference.child("generados").child(cTipo).child(cEstatus).child(cIdOrden).get()
                 .addOnCompleteListener(task -> {
-
                     btnBuscar.setEnabled(true);
                     pbCarga.setVisibility(View.INVISIBLE);
                     if(task.isSuccessful())
@@ -129,7 +203,8 @@ public class BuscarActivity extends AppCompatActivity {
                             EsconderTeclado();
                             String[] arrayDetalles;
                             List<String> lstDetalles= new ArrayList<>();
-                            Pedido pedido=task.getResult().getValue(Pedido.class);
+                            pedido=task.getResult().getValue(Pedido.class);
+                            pedido.cKey=task.getResult().getKey();
                             tvTelefono.setText(pedido.cTelefono.isEmpty()?" *** ":pedido.cTelefono);
                             tvCliente.setText(pedido.cMesa.isEmpty()?pedido.cNombreCliente:"Mesa # "+pedido.cMesa);
                             tvDireccion.setText(pedido.cDireccion.isEmpty()?" *** ":pedido.cDireccion);
@@ -140,7 +215,6 @@ public class BuscarActivity extends AppCompatActivity {
                         }
                         else
                         {
-
                             Toast.makeText(this, "No se encontró detalles de pedido", Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -161,17 +235,18 @@ public class BuscarActivity extends AppCompatActivity {
     private void Buscar(String cNumeroOrden) {
         pbCarga.setVisibility(View.VISIBLE);
         btnBuscar.setEnabled(false);
-        HashMap<String, String> hashMapData= new HashMap<>();
         databaseReference.child("todo").child(cNumeroOrden)
                 .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DataSnapshot> task) {
-                        String cEstatus, cTipo, cIdOrden;
+                        String cEstatus, cTipo;
                         if(task.isSuccessful())
                         {
                             if(task.getResult().exists())
                             {
+
                                 DataSnapshot dataSnapshot=task.getResult();
+                                String cNumeroOrdenG=dataSnapshot.getKey();
                                 cEstatus=dataSnapshot
                                         .child("cEstatus")
                                         .getValue()==null?"recibido":dataSnapshot
@@ -180,15 +255,17 @@ public class BuscarActivity extends AppCompatActivity {
                                         .child("cTipo")
                                         .getValue()==null?"comedor":dataSnapshot
                                         .child("cTipo").getValue().toString();
-                                cIdOrden=dataSnapshot.getKey();
-                                hashMapData.put("cEstatus", cEstatus);
-                                hashMapData.put("cTipo", cTipo);
-                                hashMapData.put("cIdOrden", cIdOrden);
+
+                                hashInfo.put("cEstatus", cEstatus);
+                                hashInfo.put("cTipo", cTipo);
+                                hashInfo.put("cIdOrden", cNumeroOrdenG);
+                                ActivaDesactivaMenu(cEstatus);
                                 tvEstatus.setText(cEstatus.toUpperCase(Locale.ROOT));
                                 imageViewTipo.setImageDrawable(getDrawable(getImageResourceId(cTipo)));
-                                ObtenerDetalles(hashMapData);
+                                ObtenerDetalles();
                             }
                             else {
+
                                 btnBuscar.setEnabled(true);
                                 pbCarga.setVisibility(View.INVISIBLE);
                                 Toast.makeText(BuscarActivity.this, "No se encontró el pedido, verifica el # de orden", Toast.LENGTH_SHORT).show();
@@ -203,6 +280,28 @@ public class BuscarActivity extends AppCompatActivity {
                 });
 
     }
+
+    private void ActivaDesactivaMenu(String cEstatus) {
+        switch (cEstatus)
+        {
+            case "recibido":
+                popupMenu.getMenu().getItem(1).setVisible(false);
+                popupMenu.getMenu().getItem(0).setVisible(true);
+                popupMenu.getMenu().getItem(2).setVisible(true);
+                break;
+            case "cancelado":
+                popupMenu.getMenu().getItem(1).setVisible(true);
+                popupMenu.getMenu().getItem(0).setVisible(false);
+                popupMenu.getMenu().getItem(2).setVisible(false);
+                break;
+            case "tomado":
+                popupMenu.getMenu().getItem(1).setVisible(true);
+                popupMenu.getMenu().getItem(0).setVisible(false);
+                popupMenu.getMenu().getItem(2).setVisible(true);
+                break;
+        }
+    }
+
     private int getImageResourceId(String cTipo)
     {
         int iRetorno=R.drawable.ic_comedor;
